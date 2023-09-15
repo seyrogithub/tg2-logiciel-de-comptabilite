061530ff65a06f444d452725d4110385e61e7204
seyrogithub <144135572+seyrogithub@users.noreply.github.com>
2023-09-15 11:24:08 +0200

    commit 1

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+float TVA(float prixHT) {
+    return prixHT*1.2;
+}
+
+
+int main (){
+    float prixBase;
+    printf("Veuillez rentrer un prix HT : \n");
+    scanf("%f",&prixBase);
+    prixBase = TVA(prixBase);
+    printf("Voila le prix avec la TVA : %.2f \n", TVA(prixBase));
     return 0;
 }
